369b4565db8686a3939851359959a9731272e720
Fergago20 <fergomez@uamv.edu.ni>
2025-11-19 15:00:48 -0600

    Segundo ejercicio

---
+++ Ejercicio2.java
@@ -0,0 +1,30 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+class Main {
+   
+   public static int countVowels(String text){
+       
+       int largo = text.length();
+       
+       int contador =0;
+       for (int i = 0; i < largo; i++){
+           char c = text.charAt(i);
+           if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contador++;
+           }
+       }
+       return contador;
+       
+   }
+    
+    public static void main(String[] args) {
+        
+        
+        
+        int contador = countVowels("Fernando");
+        System.out.print(contador);
+    }
+}
